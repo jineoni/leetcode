03dbfda8199b5b4a3589fe5e62f79f76034d57d9
Jiyoon Shin <149392988+jineoni@users.noreply.github.com>
2026-07-01 11:51:26 +0900

    Time: 16 ms (11.44%), Space: 20.5 MB (23.6%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        pt = 0
+        cnt = 0
+        while pt < len(nums):
+            if nums[pt] == 0:
+                nums.pop(pt)
+                cnt += 1
+            else:
+                pt += 1
+        for _ in range(cnt):
+            nums.append(0)
+        return nums
