bb85b0bf73aa43f3dee7fd27ee542923097ae2d0
Jiyoon Shin <149392988+jineoni@users.noreply.github.com>
2026-07-20 22:33:39 +0900

    Time: 0 ms (100%), Space: 19.3 MB (44.61%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -5,15 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+    def getSeq(self, stack, seq):
+        while stack:
+            curr = stack.pop()
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+            if not curr.right and not curr.left:
+                seq.append(curr.val)
+        return seq
+
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def leaves(node):
-            if not node:
-                return
-            if not node.left and not node.right:
-                yield node.val
-                return
-            yield from leaves(node.left)
-            yield from leaves(node.right)
-        
-        return list(leaves(root1)) == list(leaves(root2))
+        seq1, seq2 = [], []
+        stack1, stack2 = [root1], [root2]
+        self.getSeq(stack1, seq1)
+        self.getSeq(stack2, seq2)
+
+        if seq1 == seq2:
+            return True
+        else:
+            return False
+
         
