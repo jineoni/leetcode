ec253a810cc8d4b669f9c8bd0956d225830a1964
Jiyoon Shin <149392988+jineoni@users.noreply.github.com>
2026-07-25 17:57:55 +0900

    Time: 6 ms (64.73%), Space: 19.8 MB (78.64%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -1,26 +1,36 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        if not root:
-            return 0
+from collections import defaultdict
 
-        stack = [(root, [root.val])]
-        cnt = 0
-        while stack:
-            curr, path = stack.pop()
-            path2 = path.copy()
-            while path2:
-                pathSum = sum(path2)
-                if pathSum == targetSum:
-                    cnt += 1
-                path2.pop(0)
-            if curr.left:
-                stack.append((curr.left, path + [curr.left.val]))
-            if curr.right:
-                stack.append((curr.right, path + [curr.right.val]))
-        return cnt
+class Solution:
+
+    def dfs(self, root, currSum, targetSum, prefix, ans):
+        if not root:
+            return
+
+        currSum += root.val
+
+        # Count paths ending at current node
+        ans[0] += prefix[currSum - targetSum]
+
+        # Add current prefix
+        prefix[currSum] += 1
+
+        # Explore left and right subtree
+        self.dfs(root.left, currSum, targetSum, prefix, ans)
+        self.dfs(root.right, currSum, targetSum, prefix, ans)
+
+        # Backtrack
+        prefix[currSum] -= 1
+
+
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+
+        prefix = defaultdict(int)
+
+        # Empty prefix before starting from root
+        prefix[0] = 1
+
+        ans = [0]
+
+        self.dfs(root, 0, targetSum, prefix, ans)
+
+        return ans[0]
