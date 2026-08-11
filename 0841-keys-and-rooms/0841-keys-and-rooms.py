24b7c9860df49201ca61a14992a0c697ccb9be94
Jiyoon Shin <149392988+jineoni@users.noreply.github.com>
2026-08-11 16:28:53 +0900

    Time: 0 ms (100%), Space: 19.7 MB (62.24%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -2,13 +2,14 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         stack = [0]
         visited = [False] * len(rooms)
+        visited[0] = True
 
         while stack:
             curr = stack.pop()
-            visited[curr] = True
             for i in rooms[curr]:
                 if not visited[i]:
                     stack.append(i)
+                    visited[i] = True
         
         if sum(visited) == len(rooms):
             return True
