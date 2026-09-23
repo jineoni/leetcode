eb235fa741255835450adbc15f9c108ba2a74629
Jiyoon Shin <149392988+jineoni@users.noreply.github.com>
2026-09-22 21:24:44 -0400

    Time: 67 ms (42.64%), Space: 45.8 MB (5.43%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,29 +1,43 @@
+# class MyHashMap:
+
+#     def __init__(self):
+#         self.bucket = 10007
+#         self.hashmap = [[] for _ in range(self.bucket)]
+
+#     def put(self, key: int, value: int) -> None:
+#         index = key % self.bucket
+#         if self.hashmap[index]:
+#             for pair in self.hashmap[index]:
+#                 if pair[0] == key:
+#                     self.hashmap[index].remove(pair)
+        
+#         self.hashmap[index].append((key, value))
+        
+#     def get(self, key: int) -> int:
+#         index = key % self.bucket
+#         for pair in self.hashmap[index]:
+#             if pair[0] == key: return pair[1]
+#         return -1
+
+#     def remove(self, key: int) -> None:
+#         index = key % self.bucket
+#         for pair in self.hashmap[index]:
+#             if pair[0] == key:
+#                 self.hashmap[index].remove(pair)
+
 class MyHashMap:
 
     def __init__(self):
-        self.bucket = 10007
-        self.hashmap = [[] for _ in range(self.bucket)]
+        self.hashmap = [-1] * (10**6+1)
 
     def put(self, key: int, value: int) -> None:
-        index = key % self.bucket
-        if self.hashmap[index]:
-            for pair in self.hashmap[index]:
-                if pair[0] == key:
-                    self.hashmap[index].remove(pair)
-        
-        self.hashmap[index].append((key, value))
+        self.hashmap[key] = value
         
     def get(self, key: int) -> int:
-        index = key % self.bucket
-        for pair in self.hashmap[index]:
-            if pair[0] == key: return pair[1]
-        return -1
+        return self.hashmap[key]
 
     def remove(self, key: int) -> None:
-        index = key % self.bucket
-        for pair in self.hashmap[index]:
-            if pair[0] == key:
-                self.hashmap[index].remove(pair)
+        self.hashmap[key] = -1
 
 
 
